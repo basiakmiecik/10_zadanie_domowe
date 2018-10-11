82550e8a71c7a0790ba61b762da9ab023811fdb6
Basia Kmiecik <basiakmiecik@gmail.com>
2018-10-11 12:20:15 +0200

    dodanie wyjatkow kontrolowanych

---
+++ src/Person.java
@@ -0,0 +1,54 @@
+public class Person{
+    private String firstName;
+    private String lastName;
+    private String age;
+    private String pesel;
+
+    public Person() {
+    }
+
+    public Person(String firstName, String lastName, String age, String pesel) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+        this.pesel = pesel;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        if(firstName.length()<=2 || firstName==null)
+            throw new NameUndefinedException();
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        if(lastName.length()<=2 || lastName==null)
+            throw new NameUndefinedException();
+        this.lastName = lastName;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        if(age.equals("0"))
+            throw new IncorrectAgeException();
+        this.age = age;
+    }
+
+    public String getPesel() {
+        return pesel;
+    }
+
+    public void setPesel(String pesel) {
+        this.pesel = pesel;
+    }
+}
---
+++ src/PersonRead.java
@@ -0,0 +1,39 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class PersonRead {
+    public static void main(String[] args) throws IOException {
+
+        FileReader fileReader = new FileReader("Data_person.txt");
+        BufferedReader bufferedReader = new BufferedReader(fileReader);
+        String[] personData = new String[4];
+        String line;
+        Person person= new Person();
+            do{
+                try {
+                    person.setFirstName(bufferedReader.readLine());
+                }catch (NameUndefinedException e){
+                    System.err.println("Nie podano imienia lub podane imię składa się z jednej litery");
+                }
+                try {
+                    person.setLastName(bufferedReader.readLine());
+                }catch (NameUndefinedException e){
+                    System.err.println("Nie podano nazwiska lub podane nazwisko składa się z jednej litery");
+                }
+                try {
+                    person.setAge(bufferedReader.readLine());
+                }catch (IncorrectAgeException e){
+                    System.err.println("Nie podano wieku, lub podany wiek jest mniejszy od 1");
+                }
+            person.setPesel(bufferedReader.readLine());
+        }while((line=bufferedReader.readLine()) != null);
+
+
+            bufferedReader.close();
+
+        System.out.println(person.getFirstName()+ " "+person.getLastName()+" "+person.getAge()+" "+person.getPesel());
+        }
+    }
+
